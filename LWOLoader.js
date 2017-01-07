0d0021d6749ce0bc0a963c1618588046fd4807c1
Marcus Bizal <hello@marcbizal.com>
2017-01-07 16:46:10 -0500

    LWOLoader now returns a THREE.js Mesh
    + index.html renders a model passed to LWOLoader.
    (not included for copyright reasons)

--- LWOLoader.js
+++ LWOLoader.js
@@ -12,6 +12,8 @@
 		THREE.LWOLoader = function( manager ) {
 
 			this.manager = ( manager !== undefined ) ? manager : THREE.DefaultLoadingManager;
+			this.materials = [];
+			this.geometry = new THREE.BufferGeometry();
 
 		};
 
@@ -33,9 +35,8 @@
 
 			},
 
-			parse: function( data ) {
-
-				var view = new DataView(data)
+			parse: function( buffer ) {
+				var view = new DataView(buffer);
 
 				function fourCCToInt32( value ) {
 
@@ -56,6 +57,20 @@
 					);
 
 				}
+
+				// TYPE SIZES IN BYTES
+				// NOTE: These are only for the sake of code clarity
+				const ID4_SIZE 		= 4;
+				const I1_SIZE 		= 1;
+				const I2_SIZE 		= 2;
+				const F4_SIZE 		= 4;
+
+				const COL4_SIZE 	= 4;
+				const VEC12_SIZE 	= 12;
+				const IP2_SIZE 		= 2;
+				const FP4_SIZE 		= 4;
+				const DEG4_SIZE 	= 4;
+
 
 				// HEADER SPEC //
 
@@ -79,7 +94,7 @@
 				}
 
 				if ( view.getInt32( OFF_MAGIC ) !== LWO_MAGIC_LRR && view.getInt32( OFF_MAGIC ) !== LWO_MAGIC ) {
-					var magic = new TextDecoder().decode(new Uint8Array(data, OFF_MAGIC, 4));
+					var magic = new TextDecoder().decode(new Uint8Array(buffer, OFF_MAGIC, 4));
 					console.error( 'THREE.LWO2Loader.parse: Invalid magic ID (' + magic + ') in LWO header.' );
 					return;
 				}
@@ -93,7 +108,8 @@
 				const LWO_PCHS = 0x50434853;
 				const LWO_SURF = 0x53555246;
 
-				const HEADER_SIZE = 8;
+				const CHUNK_HEADER_SIZE = 8;
+				const SUBCHUNK_HEADER_SIZE = 6;
 
 				var geometry = new THREE.BufferGeometry();
 				var vertices = null;
@@ -107,7 +123,7 @@
 						var chunkType = view.getInt32( cursor );
 						var chunkSize = view.getInt32( cursor + 4 );
 
-						cursor += HEADER_SIZE;
+						cursor += CHUNK_HEADER_SIZE;
 
 						switch(chunkType) {
 							case LWO_PNTS:
@@ -123,42 +139,77 @@
 									points[i] = view.getFloat32( cursor + (i * 4) );
 								}
 
-								var geometry = new THREE.BufferGeometry();
-								geometry.addAttribute( 'position', new THREE.BufferAttribute( points, 3 ) );
+								this.geometry.addAttribute( 'position', new THREE.BufferAttribute( points, 3 ) );
 
-								console.dir(geometry);
-								console.dir(points);
+								//var _geometry = new THREE.Geometry().fromBufferGeometry( this.geometry );
+								//console.log(geometry);
+								this.geometry.clearGroups();
+								console.log(points);
 
 								break;
 							case LWO_SFRS:
-								var textChunk = new TextDecoder().decode(new Uint8Array(data, cursor, chunkSize));
+								var textChunk = new TextDecoder().decode(new Uint8Array(buffer, cursor, chunkSize));
 								var surfaceNames = textChunk.split('\0').filter(function(s) { return s != ''; });
 
-								console.log(surfaceNames);
+								for (i = 0; i < surfaceNames.length; i++) { 
+									var new_material = new THREE.MeshBasicMaterial();
+									new_material.name = surfaceNames[i];
+									this.materials.push(new_material);
+								}
 
 								break;
 							case LWO_POLS:
+
+								// Gather some initial data so that we can get the proper size 
+								var totalNumIndices = 0;
 								var offset = 0;
 								while (offset < chunkSize) {
-									var numVert = view.getInt16( cursor + offset );
-									var indices = new Int16Array(numVert);
+									var numIndices = view.getInt16( cursor + offset );
+									var materialIndex = view.getInt16( cursor + offset + 2 + (numIndices * 2));
 
-									for (var i = 0; i <= numVert; i++) {
-										indices[i] = view.getInt16( cursor + offset + (i*2) );
-									}			
+									this.geometry.addGroup(totalNumIndices, (numIndices - 2) * 3, materialIndex-1);
 
-									var triFanIndices = [];
-									for (var i = 0; i < numVert-2; i++) {
-										triFanIndices.push(indices[0]);
-										triFanIndices.push(indices[i+1]);
-										triFanIndices.push(indices[i+2]);
+									totalNumIndices += (numIndices - 2) * 3;
+									offset += 4 + (numIndices * 2);		
+								}
+
+								offset = 0;
+								var currentIndex = 0;
+								var allIndices = new Uint16Array(totalNumIndices);
+								while (offset < chunkSize) {
+									var numIndices = view.getInt16( cursor + offset );
+
+									console.log(numIndices)
+									console.log( cursor + offset )
+
+									offset += 2;
+
+									var faceIndices = new Int16Array(numIndices);
+									for (var i = 0; i <= numIndices; i++) {
+										faceIndices[i] = view.getInt16( cursor + offset + (i*2) );
 									}
 
-									console.log(triFanIndices);
-									console.log(indices);
+									console.log(faceIndices);
 
-									offset += 4 + (numVert * 2);		
+									for (var i = 0; i < numIndices-2; i++) {
+										allIndices[currentIndex++] = faceIndices[0];
+										allIndices[currentIndex++] = faceIndices[i+1];
+										allIndices[currentIndex++] = faceIndices[i+2];
+
+										// NOTE: 	This could work if we were using a standard Geometry rather than BufferGeometry.
+										// 			Although BufferGeometry takes a bit of extra time to parse, it is more efficient to render.
+										/*
+										var face = new THREE.Face3( indices[0], indices[i+1], indices[i+2], null, null, materialIndex );
+										this.geometry.faces.push( face );
+										*/
+									}
+
+									offset += 2 + (numIndices * 2);		
 								}
+
+								console.log(allIndices);
+
+								this.geometry.setIndex(new THREE.BufferAttribute(allIndices, 1));
 								break;
 							case LWO_SURF:
 
@@ -194,47 +245,129 @@
 								/*  SURF DEFINITIONS END  */
 								/**************************/
 
+								/*****************************/
+								/* TEXTURE DEFINITIONS START */
+								/*****************************/
+
+								// Start of Definition
+								const SURF_CTEX = 0x43544558;
+								const SURF_DTEX = 0x44544558;
+								const SURF_STEX = 0x53544558;
+								const SURF_RTEX = 0x52544558;
+								const SURF_TTEX = 0x54544558;
+								const SURF_LTEX = 0x4C544558;
+								const SURF_BTEX = 0x42544558;
+
+								// Flags
+								const SURF_TFLG = 0x54464C47;
+
+								// Location and Size
+								const SURF_TSIZ = 0x5453495A;
+								const SURF_TCTR = 0x54435452;
+								const SURF_TFAL = 0x5446414C;
+								const SURF_TVEL = 0x5456454C;
+
+								// Color
+								const SURF_TCLR = 0x54434C52;
+
+								// Value
+								const SURF_TVAL = 0x54434C52;
+
+								// Bump Amplitude
+								const SURF_TAMP = 0x54414D50;
+
+								// Image Map
+								const SURF_TIMG = 0x54494D47;
+
+								// Image Alpha
+								const SURF_TALP = 0x54414C50;
+
+								// Image Wrap Options
+								const SURF_TWRP = 0x54575250;
+
+								// Antialiasing Strength
+								const SURF_TAAS = 0x54414153;
+
+								// Texture Opacity
+								const SURF_TOPC = 0x544F5043;
+
+								/*****************************/
+								/*  TEXTURE DEFINITIONS END  */
+								/*****************************/
+
 								var offset = 0;
-								while ( view.getInt16( cursor + offset ) !== 0 ) offset++;
-								var materialName = new TextDecoder().decode(new Uint8Array(data, cursor, offset));
-								var material = new THREE.Material({ name: materialName });
+								while ( view.getUint8( cursor + offset ) !== 0 ) offset++;
+								var materialName = new TextDecoder().decode(new Uint8Array(buffer, cursor, offset));
+								var material = null;
+
+								for (var m of this.materials) {
+									if (m.name == materialName) material = m;
+								}
+
+								if (!material) {
+									console.error( 'THREE.LWO2Loader.parse: Surface in SURF chunk does not exist in SRFS' );
+									return;
+								}
+								
+								var texture = null;
 
 								while (offset < chunkSize) {
-									if ( view.getUint8( cursor+offset ) === 0 ) {
+									var subchunkOffset = cursor + offset;
+									if ( view.getUint8( subchunkOffset ) === 0 ) {
 										offset++;
 									} else {
-										var subchunkType = view.getInt32( cursor + offset );
-										var subchunkSize = view.getInt32( cursor + offset + 4 );
+										var subchunkType = view.getInt32( subchunkOffset );
+										var subchunkSize = view.getInt32( subchunkOffset + 4 );
 
-										offset += HEADER_SIZE;
+										offset += SUBCHUNK_HEADER_SIZE;
 
 										switch (subchunkType) {
 											case SURF_COLR:
-												console.log("Hello COLR!");
+												var colorArray = [];
+												for (var i = 0; i < 4; i++) {
+													colorArray.push(view.getUint8(subchunkOffset+6+i) / 255);
+												}
+												console.log(new TextDecoder().decode(new Uint8Array(buffer, subchunkOffset+6, 6)));
+												console.log(colorArray)
+
+												var color_ = new THREE.Color().fromArray(colorArray);
+
+												material.color = color_;
+
 												break;
 											case SURF_FLAG:
 												break;
 
 											case SURF_LUMI:
+												var luminosity = view.getInt16(subchunkOffset) / 255;
 												break;
 											case SURF_DIFF:
+												var diffuse = view.getInt16(subchunkOffset) / 255;
 												break;
 											case SURF_SPEC:
+												var specular = view.getInt16(subchunkOffset) / 255;
 												break;
 											case SURF_REFL:
+												var reflection = view.getInt16(subchunkOffset) / 255;
 												break;
 											case SURF_TRAN:
+												var transparency = view.getInt16(subchunkOffset) / 255;
 												break;
 
 											case SURF_VLUM:
+												var luminosity = view.getFloat32(subchunkOffset);
 												break;
 											case SURF_VDIF:
+												var diffuse = view.getFloat32(subchunkOffset);
 												break;
 											case SURF_VSPC:
+												var specular = view.getFloat32(subchunkOffset);
 												break;
 											case SURF_VRFL:
+												var reflection = view.getFloat32(subchunkOffset);
 												break;
 											case SURF_VTRN:
+												var transparency = view.getFloat32(subchunkOffset);
 												break;
 
 											case SURF_GLOS:
@@ -250,25 +383,24 @@
 											case SURF_SMAN:
 												break;
 											default:
-											console.warn('Found unrecognised subchunk type ' + new TextDecoder().decode(new Uint8Array(data, cursor+offset-HEADER_SIZE, 4)) + ' at ' + cursor + offset);
+											console.warn('Found unrecognised subchunk type ' + new TextDecoder().decode(new Uint8Array(buffer, subchunkOffset-SUBCHUNK_HEADER_SIZE, 4)) + ' at ' + subchunkOffset);
 										}
 
 										offset += subchunkSize;
 									}
 								}
 
-								console.log('Surface name: ' + materialName);
-
-
 								break;
 							default:
-							console.warn('Found unrecognised chunk type ' + new TextDecoder().decode(new Uint8Array(data, cursor-8, 4)) + ' at ' + cursor);
+							console.warn('Found unrecognised chunk type ' + new TextDecoder().decode(new Uint8Array(buffer, cursor-8, 4)) + ' at ' + cursor);
 						}
 
 						cursor += chunkSize;
 					}
 
 				}
+
+				return new THREE.Mesh(this.geometry, new THREE.MultiMaterial( this.materials ));
 			}
 		};
 
